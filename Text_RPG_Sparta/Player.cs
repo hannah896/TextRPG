92277d58e383a3b2f5728ba494c30abbe22d955a
hannah896 <phn2260@naver.com>
2025-01-31 19:50:41 +0900

    initial commit

---
+++ Text_RPG_Sparta/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Player
+{
+	private string Name = "";
+    private string Chad = "";
+    private int LV = 0;
+    private	int STR = 0;
+	private int HP = 0;
+	private int DEF = 0;
+	private int Gold = 0;
+
+	//생성자
+	public Player()
+	{
+		this.LV = 1;
+		this.Chad = "전사";
+		this.HP = 100;
+		this.DEF = 5;
+		this.STR = 10;
+		this.Gold = 1500;
+	}
+
+	public void Show_State()
+	{
+		while (true)
+		{
+			Console.WriteLine($"Lv. {this.LV}");
+			Console.WriteLine($"공격력: {this.STR}");
+			Console.WriteLine($"방어력: {this.DEF}");
+			Console.WriteLine($"체 력 : {this.LV}");
+			Console.WriteLine($"Gold  : {this.LV}");
+			Console.WriteLine();
+			Console.WriteLine("0. 나가기");
+        }
+	}
+
+	public void Show_Inventory()
+	{
+		
+	}
+}
---
+++ Text_RPG_Sparta/Start.cs
@@ -0,0 +1,20 @@
+﻿namespace Text_RPG_Sparta
+{
+    internal class Start
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+
+            int order;
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
