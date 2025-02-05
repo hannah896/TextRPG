0c504da99b590df6bc9ca1f341e527b89b2e99a4
hannah896 <phn2260@naver.com>
2025-02-05 09:37:06 +0900

    🔥Docs: "Player클래스내의 불필요한 네임스페이스 삭제 및 간단한 코드 수정
    float형식 필드들의 초기 값과 생성자의 코드에서 f를 붙이지 않은 것들을 삭제하고 테스터용 생성자를 삭제했습니다.

--- Text_RPG_Sparta/Player.cs
+++ Text_RPG_Sparta/Player.cs
@@ -1,9 +1,4 @@
 ﻿#nullable enable
-using System;
-
-/// <summary>
-/// Summary description for Class1
-/// </summary>
 public class Player
 {
 	private string name = "";
@@ -12,38 +7,19 @@ public class Player
     private int level = 0;
 	private int exp = 0;
     private int maxExp = 1;
-
-    private float hp = 0;
-	private float maxHp = 0;
-
 	private int gold = 0;
 
     private float itemEffect_atk = 0f;
     private float itemEffect_def = 0f;
     private float atk = 0f;
     private float def = 0f;
+    private float hp = 0f;
+    private float maxHp = 0f;
 
     private Item[]? inventory;
 	private Item[,]? equipItem;
 
-	//생성자
-	
-	//개발자용 테스터 생성자
-	public Player()
-    {
-        this.name = "James";
-        this.level = 1;
-        this.job = "전사";
-        this.hp = 100;
-        this.maxHp = 100;
-        this.def = 5f;
-        this.atk = 10f;
-		this.gold = 50000;
-        inventory = new Item[10];
-		equipItem = new Item[2, 1];
-		this.exp = 0;
-		this.maxExp = 1;
-    }
+    //생성자
     public Player(string name, string job)
 	{
 		this.name = name;
@@ -54,19 +30,22 @@ public class Player
 		this.maxExp = 1;
         if (job == "전사")
 		{
-            this.hp = 100;
+            this.hp = 100f;
+            this.maxHp = 100f;
             this.def = 10f;
             this.atk = 10f;
         }
 		else if (job == "도적")
 		{
-            this.hp = 80;
+            this.hp = 80f;
+            this.maxHp = 80f;
             this.def = 5f;
             this.atk = 20f;
         }
 		else if (job == "마법사")
 		{
-            this.hp = 90;
+            this.hp = 90f;
+            this.maxHp = 190f;
             this.def = 10f;
             this.atk = 25f;
         }
